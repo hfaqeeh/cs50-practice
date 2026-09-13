5ff173a66be2c887a1d069042d8b2d57a9633a6f
hfaqeeh <husseinalfaqeeh@proton.me>
2026-09-13 10:19:07 +0300

    Add first python scripts

---
+++ calculator.py
@@ -0,0 +1,12 @@
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+operator = input("Choose your operator (+ - * /)")
+if operator == "+":
+    print(x + y)
+if operator == "-":
+    print(x - y)
+if operator == "*":
+    print(x * y)
+if operator == "/":
+    print(x / y)
